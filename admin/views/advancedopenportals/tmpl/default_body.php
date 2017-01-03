ee0490a2ade98c3dafe7a3ca68343b4c9e9f9a58
horus68 <pauloizidoro@gmail.com>
2017-01-03 17:26:37 +0000

    Admin settings screen not translated
    Fix #13 Admin settings screen not translated.
    Tested against AOP 2.0.1 (hotfix branch) with Joomla 3.6 clean install
    Mysql 5.6.17
    PHP: 5.5.12

--- admin/views/advancedopenportals/tmpl/default_body.php
+++ admin/views/advancedopenportals/tmpl/default_body.php
@@ -4,29 +4,29 @@ defined('_JEXEC') or die('Restricted Access');
 ?>
 
 <fieldset>
-    <legend>Portal Settings</legend>
+    <legend><?php echo JText::_('COM_ADVANCEDOPENPORTAL_PORTAL_SETTINGS');?></legend>
     <table class="adminlist">
         <tbody>
             <tr class="row0">
-		        <td>Sugar URL</td><td><input type="text" size="40" value="<?php echo $this->items->sugar_url ?>" name="sugar_url"></td>
+		        <td><?php echo JText::_('COM_ADVANCEDOPENPORTAL_CRM_URL');?></td><td><input type="text" size="40" value="<?php echo $this->items->sugar_url ?>" name="sugar_url"></td>
             </tr>
             <tr class="row1">
-                <td>Sugar Username</td><td><input type="text" value="<?php echo $this->items->sugar_user ?>" name="sugar_user"></td>
+                <td><?php echo JText::_('COM_ADVANCEDOPENPORTAL_CRM_USERNAME');?></td><td><input type="text" value="<?php echo $this->items->sugar_user ?>" name="sugar_user"></td>
             </tr>
             <tr class="row0">
-                <td>Sugar Password</td><td><input type="password" value="<?php echo $this->items->sugar_pass ?>" name="sugar_pass"></td>
+                <td><?php echo JText::_('COM_ADVANCEDOPENPORTAL_CRM_PASSWORD');?></td><td><input type="password" value="<?php echo $this->items->sugar_pass ?>" name="sugar_pass"></td>
 	        </tr>
             <tr class="row1">
-                <td><label for="allow_case_reopen">Allow reopening cases</label></td><td><input type="checkbox" <?php echo $this->items->allow_case_reopen ? 'checked="checked"' : '';?> name="allow_case_reopen" id="allow_case_reopen"></td>
+                <td><label for="allow_case_reopen"><?php echo JText::_('COM_ADVANCEDOPENPORTAL_CRM_CASES_ALLOW_REOPENING');?></label></td><td><input type="checkbox" <?php echo $this->items->allow_case_reopen ? 'checked="checked"' : '';?> name="allow_case_reopen" id="allow_case_reopen"></td>
             </tr>
             <tr class="row0">
-                <td><label for="allow_case_closing">Allow closing cases</label></td><td><input type="checkbox" <?php echo $this->items->allow_case_closing ? 'checked="checked"' : '';?> name="allow_case_closing" id="allow_case_closing"></td>
+                <td><label for="allow_case_closing"><?php echo JText::_('COM_ADVANCEDOPENPORTAL_CRM_CASES_ALLOW_CLOSING');?></label></td><td><input type="checkbox" <?php echo $this->items->allow_case_closing ? 'checked="checked"' : '';?> name="allow_case_closing" id="allow_case_closing"></td>
             </tr>
             <tr class="row1">
-                <td><label for="allow_priority">Allow choosing case priority</label></td><td><input type="checkbox" <?php echo $this->items->allow_priority ? 'checked="checked"' : '';?> name="allow_priority" id="allow_priority"></td>
+                <td><label for="allow_priority"><?php echo JText::_('COM_ADVANCEDOPENPORTAL_CRM_CASES_ALLOW_CHOOSING_PRIORITY');?></label></td><td><input type="checkbox" <?php echo $this->items->allow_priority ? 'checked="checked"' : '';?> name="allow_priority" id="allow_priority"></td>
             </tr>
             <tr class="row0">
-                <td><label for="allow_type">Allow choosing case type</label></td><td><input type="checkbox" <?php echo $this->items->allow_type ? 'checked="checked"' : '';?> name="allow_type" id="allow_type"></td>
+                <td><label for="allow_type"><?php echo JText::_('COM_ADVANCEDOPENPORTAL_CRM_CASES_ALLOW_CHOOSING_TYPE');?></label></td><td><input type="checkbox" <?php echo $this->items->allow_type ? 'checked="checked"' : '';?> name="allow_type" id="allow_type"></td>
             </tr>
         </tbody>
     </table>
